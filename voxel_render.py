9b732951d842555488f6b1b8372d6e10eca35edb
Wokletoff <96227014+Wokletoff@users.noreply.github.com>
2022-03-14 23:21:14 +0300

    remove unnecessary drawing

--- voxel_render.py
+++ voxel_render.py
@@ -16,6 +16,42 @@ map_width = len(height_map)
 @njit(fastmath=True)
 def ray_casting(screen_array, player_pos, player_angle, player_height, player_pitch,
                 screen_width, screen_height, delta_angle, ray_distance, h_fov, scale_height):
+
+    screen_array[:] = np.array([0, 0, 0])
+    y_buffer = np.full(screen_width, screen_height)
+
+    ray_angle = player_angle - h_fov
+    for num_ray in range(screen_width):
+        first_contact = False
+        sin_a = math.sin(ray_angle)
+        cos_a = math.cos(ray_angle)
+
+        for depth in range(1, ray_distance):
+            x = int(player_pos[0] + depth * cos_a)
+            if 0 < x < map_width:
+                y = int(player_pos[1] + depth * sin_a)
+                if 0 < y < map_height:
+
+                    # remove fish eye and get height on screen
+                    depth *= math.cos(player_angle - ray_angle)
+                    height_on_screen = int((player_height - height_map[x, y][0]) /
+                                           depth * scale_height + player_pitch)
+
+                    # remove unnecessary drawing
+                    if not first_contact:
+                        y_buffer[num_ray] = min(height_on_screen, screen_height)
+                        first_contact = True
+
+                    # remove mirror bug
+
+                    # draw vert line
+                    if height_on_screen < y_buffer[num_ray]:
+                        for screen_y in range(height_on_screen, y_buffer[num_ray]):
+                            screen_array[num_ray, screen_y] = color_map[x, y]
+                        y_buffer[num_ray] = height_on_screen
+
+        ray_angle += delta_angle
+
     return screen_array
 
 
