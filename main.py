77aeed74de1d1d63d0ec0336d8fd1afdd5a30b35
Wokletoff <96227014+Wokletoff@users.noreply.github.com>
2022-03-31 20:34:21 +0300

    refresh draw function in menu class

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import pygame as pg
 from player import Player
 from voxel_render import VoxelRender
-import sys
-from menu import menu
-from random import randrange
+from menu import Menu
 
 
 class App:
@@ -12,66 +10,17 @@ class App:
         self.screen = pg.display.set_mode(self.res, pg.SCALED)
         self.clock = pg.time.Clock()
         self.player = Player()
+        self.menu = Menu()
         self.voxel_render = VoxelRender(self)
-        # menu
-        self.menu_trigger = True
-        self.menu_picture = pg.image.load("bcgd.jpg").convert()
-        # font
-        self.font = pg.font.SysFont("Arial", 36, bold=True)
 
     def update(self):
         self.player.update()
         self.voxel_render.update()
 
     def draw(self):
-        x = 0
-        button_font = pg.font.Font('font.ttf', 72)
-        label_font = pg.font.Font('font1.otf', 400)
-        start = button_font.render('START', 1, pg.Color('lightgray'))
-        button_start = pg.Rect(0, 0, 400, 150)
-        button_start.center = 800, 450
-        exit = button_font.render('EXIT', 1, pg.Color('lightgray'))
-        button_exit = pg.Rect(0, 0, 400, 150)
-        button_exit.center = 800, 450 + 200
-
-        while self.menu_trigger:
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    pg.quit()
-                    sys.exit()
-
-            self.screen.blit(self.menu_picture, (0, 0), (x % 1200, 400, 1200, 800))
-            x += 1
-
-            pg.draw.rect(self.screen, (0, 0, 0), button_start, border_radius=25, width=10)
-            self.screen.blit(start, (button_start.centerx - 130, button_start.centery - 70))
-
-            pg.draw.rect(self.screen, (0, 0, 0), button_exit, border_radius=25, width=10)
-            self.screen.blit(exit, (button_exit.centerx - 85, button_exit.centery - 70))
-
-            color = randrange(40)
-
-            label = label_font.render('GroundMap', 1, (color, color, color))
-            self.screen.blit(label, (15, -30))
-
-            mouse_pos = pg.mouse.get_pos()
-            mouse_click = pg.mouse.get_pressed()
-            if button_start.collidepoint(mouse_pos):
-                pg.draw.rect(self.screen, color, button_start, border_radius=25)
-                self.screen.blit(start, (button_start.centerx - 130, button_start.centery - 70))
-                if mouse_click[0]:
-                    # action at klick
-                    self.menu_trigger = False
-
-            elif button_exit.collidepoint(mouse_pos):
-                pg.draw.rect(self.screen, color, button_exit, border_radius=25)
-                self.screen.blit(exit, (button_exit.centerx - 85, button_exit.centery - 70))
-                if mouse_click[0]:
-                    pg.quit()
-                    sys.exit()
-
-            pg.display.flip()
-            self.clock.tick(20)
+        self.menu.update()
+        self.voxel_render.draw()
+        pg.display.flip()
 
     def run(self):
         while True:
@@ -85,6 +34,5 @@ class App:
 
 if __name__ == "__main__":
     pg.init()
-    pg.mouse.set_visible(False)
     app = App()
     app.run()
