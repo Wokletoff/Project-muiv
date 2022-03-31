eba69eb045a441867c5faac6578bdb7c948a77de
Wokletoff <96227014+Wokletoff@users.noreply.github.com>
2022-03-31 20:09:12 +0300

    Add animation background

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from random import randrange
 
 class App:
     def __init__(self):
-        self.res = self.width, self.height = (1920, 1080)
+        self.res = self.width, self.height = (1200, 800)
         self.screen = pg.display.set_mode(self.res, pg.SCALED)
         self.clock = pg.time.Clock()
         self.player = Player()
@@ -17,13 +17,14 @@ class App:
         self.menu_trigger = True
         self.menu_picture = pg.image.load("bcgd.jpg").convert()
         # font
-        self.font = pg.font.SysFont("", 36, bold=True)
+        self.font = pg.font.SysFont("Arial", 36, bold=True)
 
     def update(self):
         self.player.update()
         self.voxel_render.update()
 
     def draw(self):
+        x = 0
         button_font = pg.font.Font('font.ttf', 72)
         label_font = pg.font.Font('font1.otf', 400)
         start = button_font.render('START', 1, pg.Color('lightgray'))
@@ -32,18 +33,23 @@ class App:
         exit = button_font.render('EXIT', 1, pg.Color('lightgray'))
         button_exit = pg.Rect(0, 0, 400, 150)
         button_exit.center = 800, 450 + 200
+
         while self.menu_trigger:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
-                    self.menu_trigger = False
+                    pg.quit()
+                    sys.exit()
 
-            color = randrange(40)
+            self.screen.blit(self.menu_picture, (0, 0), (x % 1200, 400, 1200, 800))
+            x += 1
 
-            pg.draw.rect(self.screen, color, button_start, border_radius=25, width=10)
+            pg.draw.rect(self.screen, (0, 0, 0), button_start, border_radius=25, width=10)
             self.screen.blit(start, (button_start.centerx - 130, button_start.centery - 70))
 
-            pg.draw.rect(self.screen, color, button_exit, border_radius=25, width=10)
+            pg.draw.rect(self.screen, (0, 0, 0), button_exit, border_radius=25, width=10)
             self.screen.blit(exit, (button_exit.centerx - 85, button_exit.centery - 70))
+
+            color = randrange(40)
 
             label = label_font.render('GroundMap', 1, (color, color, color))
             self.screen.blit(label, (15, -30))
@@ -55,8 +61,7 @@ class App:
                 self.screen.blit(start, (button_start.centerx - 130, button_start.centery - 70))
                 if mouse_click[0]:
                     # action at klick
-                        self.voxel_render.draw()
-                        pg.display.flip()
+                    self.menu_trigger = False
 
             elif button_exit.collidepoint(mouse_pos):
                 pg.draw.rect(self.screen, color, button_exit, border_radius=25)
