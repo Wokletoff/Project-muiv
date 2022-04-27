b526c40c31e3f163f6597bb1475cf7fd0b610cdd
509 <a@a.b>
2022-04-27 15:21:40 +0300

    add help button

--- menu.py
+++ menu.py
@@ -37,6 +37,11 @@ class Menu:
         button_exit = pg.Rect(0, 0, 400, 150)
         button_exit.center = 600, 450 + 250
 
+        help = button_font.render('HELP', 1, pg.Color('lightgray'))
+        button_help = pg.Rect(0, 0, 300, 150)
+        button_help.center = 1020, 450 + 250
+
+        next_function = None
         while self.menu_trigger:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
@@ -48,6 +53,9 @@ class Menu:
 
             pg.draw.rect(self.screen, (0, 0, 0), button_start, border_radius=25, width=10)
             self.screen.blit(start, (button_start.centerx - 120, button_start.centery - 60))
+
+            pg.draw.rect(self.screen, (0, 0, 0), button_help, border_radius=25, width=10)
+            self.screen.blit(help, (button_help.centerx - 105, button_help.centery - 68))
 
             pg.draw.rect(self.screen, (0, 0, 0), button_about, border_radius=25, width=10)
             self.screen.blit(about, (button_about.centerx - 100, button_about.centery - 60))
@@ -69,89 +77,101 @@ class Menu:
                 pg.draw.rect(self.screen, color, button_start, border_radius=25)
                 self.screen.blit(start, (button_start.centerx - 120, button_start.centery - 60))
                 if mouse_click[0]:
-                    # action at click
                     app = App()
-                    app.run()
+                    self.menu_trigger = False
+                    next_function = app.run
 
             elif button_options.collidepoint(mouse_pos):
                 pg.draw.rect(self.screen, color, button_options, border_radius=25)
                 self.screen.blit(options, (button_options.centerx - 150, button_options.centery - 60))
                 if mouse_click[0]:
-                    # action at click
-                    self.option()
                     self.menu_trigger = False
+                    next_function = self.option
 
             elif button_about.collidepoint(mouse_pos):
                 pg.draw.rect(self.screen, color, button_about, border_radius=25)
                 self.screen.blit(about, (button_about.centerx - 100, button_about.centery - 60))
                 if mouse_click[0]:
-                    # action at click
-                    self.credits()
                     self.menu_trigger = False
+                    next_function = self.credits
+
+            elif button_help.collidepoint(mouse_pos):
+                pg.draw.rect(self.screen, color, button_help, border_radius=25)
+                self.screen.blit(help, (button_help.centerx - 150, button_help.centery - 60))
+                if mouse_click[0]:
+                    # action at click
+                    self.menu_trigger = False
+                    next_function = self.help
 
             elif button_exit.collidepoint(mouse_pos):
                 pg.draw.rect(self.screen, color, button_exit, border_radius=25)
                 self.screen.blit(exit, (button_exit.centerx - 80, button_exit.centery - 60))
                 if mouse_click[0]:
+                    pg.event.post(pg.event.Event(pg.QUIT))
+
+            pg.display.flip()
+            self.clock.tick(20)
+        if next_function:
+            self.menu_trigger = True
+            next_function()
+
+    def option(self):
+        x = 0
+        button_font = pg.font.Font('font/font.ttf', 72)
+        label_font = pg.font.Font('font/font1.otf', 350)
+
+        volume = button_font.render('Volume', 1, pg.Color('lightgray'))
+        button_volume = pg.Rect(0, 0, 600, 150)
+        button_volume.center = 600, 450 - 100
+
+        back = button_font.render('BACK', 1, pg.Color('lightgray'))
+        button_back = pg.Rect(0, 0, 250, 100)
+        button_back.center = 1070, 450 + 280
+
+        next_function = None
+
+        while self.menu_trigger:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
                     pg.quit()
                     sys.exit()
+
+            self.screen.blit(self.menu_picture, (0, 0), (x % 1200, 400, 1200, 800))
+            x += 1
+
+            pg.draw.rect(self.screen, (0, 0, 0), button_volume, border_radius=25, width=10)
+            self.screen.blit(volume, (button_volume.centerx - 240, button_volume.centery - 68))
+
+            pg.draw.rect(self.screen, (0, 0, 0), button_back, border_radius=25, width=10)
+            self.screen.blit(back, (button_back.centerx - 105, button_back.centery - 68))
+
+            color = randrange(40)
+
+            label = label_font.render('Options', 1, (color, color, color))
+            self.screen.blit(label, (15, -30))
+
+            mouse_pos = pg.mouse.get_pos()
+            mouse_click = pg.mouse.get_pressed()
+
+            if button_back.collidepoint(mouse_pos):
+                pg.draw.rect(self.screen, color, button_back, border_radius=25)
+                self.screen.blit(back, (button_back.centerx - 105, button_back.centery - 68))
+                if mouse_click[0]:
+                    # action at click
+                    self.menu_trigger = False
+                    next_function = self.run
+
+            elif button_volume.collidepoint(mouse_pos):
+                pg.draw.rect(self.screen, color, button_volume, border_radius=25)
+                self.screen.blit(volume, (button_volume.centerx - 240, button_volume.centery - 68))
+                if mouse_click[0]:
+                    # action at click
+                    pass
 
             pg.display.flip()
             self.clock.tick(20)
 
-    def option(self):
-             x = 0
-             button_font = pg.font.Font('font/font.ttf', 72)
-             label_font = pg.font.Font('font/font1.otf', 350)
-
-             volume = button_font.render('Volume', 1, pg.Color('lightgray'))
-             button_volume = pg.Rect(0, 0, 600, 150)
-             button_volume.center = 600, 450 - 100
-
-             back = button_font.render('BACK', 1, pg.Color('lightgray'))
-             button_back = pg.Rect(0, 0, 250, 100)
-             button_back.center = 1070, 450 + 280
-
-             while self.menu_trigger:
-                  for event in pg.event.get():
-                     if event.type == pg.QUIT:
-                         pg.quit()
-                         sys.exit()
-
-                  self.screen.blit(self.menu_picture, (0, 0), (x % 1200, 400, 1200, 800))
-                  x += 1
-
-                  pg.draw.rect(self.screen, (0, 0, 0), button_volume, border_radius=25, width=10)
-                  self.screen.blit(volume, (button_volume.centerx - 240, button_volume.centery - 68))
-
-                  pg.draw.rect(self.screen, (0, 0, 0), button_back, border_radius=25, width=10)
-                  self.screen.blit(back, (button_back.centerx - 105, button_back.centery - 68))
-
-                  color = randrange(40)
-
-                  label = label_font.render('Options', 1, (color, color, color))
-                  self.screen.blit(label, (15, -30))
-
-                  mouse_pos = pg.mouse.get_pos()
-                  mouse_click = pg.mouse.get_pressed()
-
-                  if button_back.collidepoint(mouse_pos):
-                    pg.draw.rect(self.screen, color, button_back, border_radius=25)
-                    self.screen.blit(back, (button_back.centerx - 105, button_back.centery - 68))
-                    if mouse_click[0]:
-                        # action at click
-                        self.run()
-                        self.menu_trigger = False
-
-                  elif button_volume.collidepoint(mouse_pos):
-                    pg.draw.rect(self.screen, color, button_volume, border_radius=25)
-                    self.screen.blit(volume, (button_volume.centerx - 240, button_volume.centery - 68))
-                    if mouse_click[0]:
-                        # action at click
-                        pass
-
-                  pg.display.flip()
-                  self.clock.tick(20)
+        if next_function: next_function()
 
     def credits(self):
         x = 0
@@ -164,41 +184,98 @@ class Menu:
         button_back = pg.Rect(0, 0, 250, 100)
         button_back.center = 1070, 450 + 280
 
+        next_function = None
+
         while self.menu_trigger:
-             for event in pg.event.get():
+            for event in pg.event.get():
                 if event.type == pg.QUIT:
                     pg.quit()
                     sys.exit()
 
-             self.screen.blit(self.menu_picture, (0, 0), (x % 1200, 400, 1200, 800))
-             x += 1
+            self.screen.blit(self.menu_picture, (0, 0), (x % 1200, 400, 1200, 800))
+            x += 1
 
-             color = randrange(40)
+            color = randrange(40)
 
-             label = label_font.render('CREDIT', 1, (color, color, color))
-             self.screen.blit(label, (100, -30))
+            label = label_font.render('CREDIT', 1, (color, color, color))
+            self.screen.blit(label, (100, -30))
 
-             text1 = text_font.render("!!!Thanks for play!!!", 1, (color, color, color))
-             self.screen.blit(text1, (40, 350))
+            text1 = text_font.render("!!!Thanks for play!!!", 1, (color, color, color))
+            self.screen.blit(text1, (40, 350))
 
-             text2 = text_font2.render("Credit by Woklet", 1, (color, color, color))
-             self.screen.blit(text2, (20, 750))
+            text2 = text_font2.render("Credit by Woklet", 1, (color, color, color))
+            self.screen.blit(text2, (20, 750))
 
-             pg.draw.rect(self.screen, (0, 0, 0), button_back, border_radius=25, width=10)
-             self.screen.blit(back, (button_back.centerx - 105, button_back.centery - 68))
+            pg.draw.rect(self.screen, (0, 0, 0), button_back, border_radius=25, width=10)
+            self.screen.blit(back, (button_back.centerx - 105, button_back.centery - 68))
 
-             mouse_pos = pg.mouse.get_pos()
-             mouse_click = pg.mouse.get_pressed()
-             if button_back.collidepoint(mouse_pos):
-                 pg.draw.rect(self.screen, color, button_back, border_radius=25)
-                 self.screen.blit(back, (button_back.centerx - 105, button_back.centery - 68))
-                 if mouse_click[0]:
-                     # action at click
-                     self.run()
-                     self.menu_trigger = False
+            mouse_pos = pg.mouse.get_pos()
+            mouse_click = pg.mouse.get_pressed()
+            if button_back.collidepoint(mouse_pos):
+                pg.draw.rect(self.screen, color, button_back, border_radius=25)
+                self.screen.blit(back, (button_back.centerx - 105, button_back.centery - 68))
+                if mouse_click[0]:
+                    # action at click
+                    self.menu_trigger = False
+                    next_function = self.run
 
-             pg.display.flip()
-             self.clock.tick(20)
+            pg.display.flip()
+            self.clock.tick(20)
+
+        if next_function:
+            self.menu_trigger = True
+            next_function()
+
+    def help(self):
+        x = 0
+        button_font = pg.font.Font('font/font.ttf', 72)
+        text_font = pg.font.Font('font/font.ttf', 40)
+        label_font = pg.font.Font('font/font1.otf', 350)
+
+        back = button_font.render('BACK', 1, pg.Color('lightgray'))
+        button_back = pg.Rect(0, 0, 250, 100)
+        button_back.center = 1070, 450 + 280
+
+        next_function = None
+
+        while self.menu_trigger:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
+                    sys.exit()
+
+            self.screen.blit(self.menu_picture, (0, 0), (x % 1200, 400, 1200, 800))
+            x += 1
+
+            color = randrange(40)
+
+            label = label_font.render('HELP', 1, (color, color, color))
+            self.screen.blit(label, (300, -30))
+
+            text1 = text_font.render("Control position - (WASD, Q, E)", 1, (color, color, color))
+            self.screen.blit(text1, (40, 350))
+
+            text2 = text_font.render("Control сamera (up, down, left, right)", 1, (color, color, color))
+            self.screen.blit(text2, (40, 400))
+
+            pg.draw.rect(self.screen, (0, 0, 0), button_back, border_radius=25, width=10)
+            self.screen.blit(back, (button_back.centerx - 105, button_back.centery - 68))
+
+            mouse_pos = pg.mouse.get_pos()
+            mouse_click = pg.mouse.get_pressed()
+
+            if button_back.collidepoint(mouse_pos):
+                pg.draw.rect(self.screen, color, button_back, border_radius=25)
+                self.screen.blit(back, (button_back.centerx - 105, button_back.centery - 68))
+                if mouse_click[0]:
+                    # action at click
+                    self.menu_trigger = False
+                    self.next_function = self.run
+
+            pg.display.flip()
+            self.clock.tick(20)
+
+        if next_function: next_function()
 
 
 if __name__ == "__main__":
